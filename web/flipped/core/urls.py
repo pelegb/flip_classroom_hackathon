625ffd5ed9e25eb8c2542fdbbfdf3db31d6b2bb5
flipped <hfntsh@ubuntu.(none)>
2013-12-13 20:19:36 +0200

    views for TeachEntity

--- web/flipped/core/urls.py
+++ web/flipped/core/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     #url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^videos/(?P<video_id>\d+)/$', views.video_detail, name='video_detail'),
-    url(r'^add-video/',views.add_video,name='add_video') 
+    url(r'^add-video/',views.add_video,name='add_video'),
+    url(r'^topic/(?P<topic_id>\d+)/$', views.topic_view, name='topic_view'),
+    url(r'^item/(?P<item_id>\d+)/$', views.item_view, name='item_view')
 )
