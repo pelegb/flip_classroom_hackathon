4504f6bb4944fb38078e9007ba8836ae1c5ac5db
Adam Matan <adam@matan.name>
2013-12-13 13:55:08 +0200

    First working view!

---
+++ web/flipped/core/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from core import views
+
+urlpatterns = patterns('',
+    #url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^videos/(?P<video_id>\d+)/$', views.video_detail, name='video_detail'),
+)
--- web/flipped/core/views.py
+++ web/flipped/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from models import VideoPage
 
 # Create your views here.
+
+def video_detail(request, video_id):
+    video = get_object_or_404(VideoPage, pk=video_id)
+    return render(request, 'core/video_detail.html', {'video': video})
