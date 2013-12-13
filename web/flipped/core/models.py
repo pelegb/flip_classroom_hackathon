7b467592921204a63e0967561a77752f3abe6d01
Adam Matan <adam@matan.name>
2013-12-13 15:06:34 +0200

    Add Alef Font

--- web/flipped/core/models.py
+++ web/flipped/core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class VideoPage(models.Model):
-    youtube_link = models.URLField()
-    upload_date  = models.DateTimeField('date uploaded to our site')
-    content      = models.TextField()
-    video_title  = models.CharField(max_length=50)
+    youtube_movie_id = models.CharField(max_length=25)
+    upload_date      = models.DateTimeField('date uploaded to our site')
+    content          = models.TextField()
+    video_title      = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.video_title
