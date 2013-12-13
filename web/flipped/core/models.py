20010a49f46d14127111dea9f6f08254d6bc1615
Adam Matan <adam@matan.name>
2013-12-13 13:55:08 +0200

    First working view!

--- web/flipped/core/models.py
+++ web/flipped/core/models.py
@@ -4,6 +4,10 @@ class VideoPage(models.Model):
     youtube_link = models.URLField()
     upload_date  = models.DateTimeField('date uploaded to our site')
     content      = models.TextField()
+    video_title  = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.video_title
 
 class Review():
     video   = models.ForeignKey(VideoPage)
