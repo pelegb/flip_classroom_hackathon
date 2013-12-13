15de4306521ceb382dbbd168204f105207f6af97
Eran Keydar <ekeydar@gmail.com>
2013-12-13 14:59:09 +0200

    user

--- web/flipped/core/models.py
+++ web/flipped/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import django.contrib.auth
 
 class VideoPage(models.Model):
     youtube_link = models.URLField()
-    upload_date  = models.DateTimeField('date uploaded to our site')
+    upload_date  = models.DateTimeField('date uploaded to our site',auto_now_add=True)
     content      = models.TextField()
     video_title  = models.CharField(max_length=50)
-
+    user = models.ForeignKey(django.contrib.auth.get_user_model())
     def __unicode__(self):
         return self.video_title
 
@@ -13,6 +14,7 @@ class Review(models.Model):
     class Meta:
         abstract=True
     video   = models.ForeignKey(VideoPage)
+    user = models.ForeignKey(django.contrib.auth.get_user_model())
     
 class RatingReview(Review):
     context_choices = (
