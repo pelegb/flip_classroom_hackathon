c88cf2e090a3a3cfdd32e696281734df46fa6dd4
Eran Keydar <ekeydar@gmail.com>
2013-12-13 17:12:41 +0200

    add_video

--- web/flipped/core/admin.py
+++ web/flipped/core/admin.py
@@ -4,6 +4,10 @@ from core import models
 admin.site.register(models.VideoPage)
 admin.site.register(models.TextualReview)
 admin.site.register(models.RatingReview)
+admin.site.register(models.TeachItem)
+admin.site.register(models.TeachTopic)
+
+
 
 
 
---
+++ web/flipped/core/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+import models
+class VideoForm(forms.Form):
+    link = forms.URLField()
+    title = forms.CharField(max_length=50)
+    content = forms.CharField(widget=forms.Textarea)
+    item = forms.ModelChoiceField(queryset=models.TeachItem.objects.all())
+    
+    
