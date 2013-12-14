952dfcf584fd4544ce1c5b1e15abfda58bf896b8
flipped <hfntsh@ubuntu.(none)>
2013-12-14 17:48:12 +0200

    hierarchy view for topics and items

--- web/flipped/core/views.py
+++ web/flipped/core/views.py
@@ -35,12 +35,14 @@ def add_video(request):
 
 def topic_view(request,topic_id):
     topic = get_object_or_404(TeachTopic, pk=topic_id)
-    topic_children = TeachTopic.objects.filter(parent=topic)
-    item_children = TeachItem.objects.filter(parent=topic)
-    return render(request, 'core/topic_view.html', {'topic': topic, 'topic_children':topic_children, 'item_children':item_children})
+    subtree = common.utils.get_subtree_from_topic(topic)
+    item_list = TeachItem.objects.filter(parent=topic)
+    ancestors = common.utils.get_ancestry_from_entity(topic)
+    return render(request, 'core/topic_view.html', {'topic': topic, 'subtree':subtree, 'ancestors': ancestors, 'item_list':item_list})
 
 def item_view(request,item_id):
     item = get_object_or_404(TeachItem, pk=item_id)
     videos = VideoPage.objects.filter(teach_item=item)
-    return render(request, 'core/item_view.html', {'item': item, 'videos':videos})
+    ancestors = common.utils.get_ancestry_from_entity(item)
+    return render(request, 'core/item_view.html', {'item': item, 'videos':videos, 'ancestors':ancestors})
 
