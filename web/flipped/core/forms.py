b6c98ce6ed6749e788f3c1156434509d2b0d0d67
Eran Keydar <ekeydar@gmail.com>
2013-12-15 01:02:12 +0200

    fix bugs

--- web/flipped/core/forms.py
+++ web/flipped/core/forms.py
@@ -4,6 +4,7 @@ from wysihtml5.widgets import Wysihtml5TextareaWidget
 import models
 
 class VideoForm(forms.Form):
+    edited_id = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     link = forms.URLField()
     title = forms.CharField(max_length=50)
     content = forms.CharField(widget=Wysihtml5TextareaWidget())
--- web/flipped/flipped/urls.py
+++ web/flipped/flipped/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'flipped.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$',RedirectView.as_view(url='/common/home/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^core/', include('core.urls', namespace="core")),
     url(r'^fc_user/',include('fc_user.urls',namespace="fc_user")),
     url(r'^common/',include('common.urls',namespace="common"))
 )
+
 
 
 
