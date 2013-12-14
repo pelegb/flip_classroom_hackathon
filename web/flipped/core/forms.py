4b4f1a5335eaa9bae5a7a576aa52aa16bf0cb9b0
Eran Keydar <ekeydar@gmail.com>
2013-12-14 18:39:00 +0200

    form

--- web/flipped/core/forms.py
+++ web/flipped/core/forms.py
@@ -6,7 +6,7 @@ import models
 class VideoForm(forms.Form):
     link = forms.URLField()
     title = forms.CharField(max_length=50)
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=Wysihtml5TextareaWidget())
     item = forms.ModelChoiceField(queryset=models.TeachItem.objects.all())     
     tags = forms.ModelMultipleChoiceField(queryset=models.Tag.objects.all())
     
