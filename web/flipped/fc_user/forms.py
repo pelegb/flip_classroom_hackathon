0198a7cf87902457951191dec48754604422bfc4
Eran Keydar <ekeydar@gmail.com>
2013-12-14 17:24:05 +0200

    add first name and last name

--- web/flipped/fc_user/forms.py
+++ web/flipped/fc_user/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    password = forms.CharField(max_length=10)
-    email = forms.EmailField()
+    username = forms.CharField(max_length=30,required=True)
+    password = forms.CharField(max_length=10,required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30,required=True)
+    last_name = forms.CharField(max_length=30,required=True)
+    
     
     
 
