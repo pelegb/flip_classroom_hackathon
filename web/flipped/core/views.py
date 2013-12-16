947e89b2991167e6a8fabc2d86f73092ace41610
Eran Keydar <ekeydar@gmail.com>
2013-12-16 19:23:08 +0200

    fix bug

--- web/flipped/core/views.py
+++ web/flipped/core/views.py
@@ -59,13 +59,13 @@ def topic_view(request,topic_id):
     item_list = TeachItem.objects.filter(parent=topic)
     ancestors = common.utils.get_ancestry_from_entity(topic)
     ancestors = ancestors[:-1]
-    return render('core/topic_view.html', {'topic': topic, 'subtree':subtree, 'ancestors': ancestors, 'item_list':item_list})
+    return render(request,'core/topic_view.html', {'topic': topic, 'subtree':subtree, 'ancestors': ancestors, 'item_list':item_list})
 
 def item_view(request,item_id):
     item = get_object_or_404(TeachItem, pk=item_id)
     videos = VideoPage.objects.filter(teach_item=item)
     ancestors = common.utils.get_ancestry_from_entity(item)
     ancestors = ancestors[:-1]
-    return render('core/item_view.html', {'item': item, 'videos':videos, 'ancestors':ancestors})
+    return render(request,'core/item_view.html', {'item': item, 'videos':videos, 'ancestors':ancestors})
 
 
