24122bcc8b03e6c8111a4ba11a7a87688e5d63ad
Eran Keydar <ekeydar@gmail.com>
2013-12-16 23:32:57 +0200

    locale for he

--- web/flipped/core/forms.py
+++ web/flipped/core/forms.py
@@ -4,21 +4,14 @@ import floppyforms as forms
 from django.conf import settings
 from wysihtml5.widgets import Wysihtml5TextareaWidget
 import models
-
-
-translations = {
-    "link" : "קישור",
-    "title": "כותרת",
-    "content": "תוכן",
-    "item": "שיוך",
-    "tags": "תגיות",
-}
+from django.utils.translation import ugettext_lazy as _
 
 class VideoForm(forms.Form):
     edited_id = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
-    link = forms.URLField(label=translations['link'])
-    title = forms.CharField(max_length=50, label=translations['title'])
-    content = forms.CharField(widget=Wysihtml5TextareaWidget(), label=translations['content'])
-    item = forms.ModelChoiceField(queryset=models.TeachItem.objects.all(),required=True, label=translations['item'])
-    tags = forms.ModelMultipleChoiceField(queryset=models.Tag.objects.all(),required=False, label=translations['tags'])
+    link = forms.URLField(label=_("link"))
+    title = forms.CharField(max_length=50,label=_("title"))
+    content = forms.CharField(widget=Wysihtml5TextareaWidget(),label=_("content"))
+    item = forms.ModelChoiceField(queryset=models.TeachItem.objects.all(),required=True, label=_("item"))
+    tags = forms.ModelMultipleChoiceField(queryset=models.Tag.objects.all(),required=False, label=_("tags"))
+    
 
