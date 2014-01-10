4fc1a1be5357f08f956ccd82f9c385bd43681505
Eran Keydar <ekeydar@gmail.com>
2014-01-10 11:36:41 +0200

    rating

--- web/flipped/common/templatetags/flip_filters.py
+++ web/flipped/common/templatetags/flip_filters.py
@@ -22,6 +22,13 @@ MONTHS = ['oops',
 def heb_month(month):
     return MONTHS[month]
 
+@register.filter(name="range")
+def get_range(i):
+    return range(i)
+
+@register.filter(name="range_to")
+def get_range_to(i,rest):
+    return range(i,rest)
 
 
 
