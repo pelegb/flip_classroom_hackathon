d0cfe455f372f48071443f269476ab490ad9617b
Adam Matan <adam@vioozer.com>
2014-02-25 09:38:30 +0200

    Add video form improvements: Add placeholder text, wider input fields

--- web/flipped/core/forms.py
+++ web/flipped/core/forms.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 from wysihtml5.widgets import Wysihtml5TextareaWidget
@@ -7,8 +9,14 @@ import models
 class VideoForm(forms.Form):
     edited_id = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     link = forms.URLField(label=_("link"),
-                          widget=forms.TextInput(attrs={'placeholder': 'https://www.youtube.com/watch?v=2H4RkudFzlc'}))
-    title = forms.CharField(max_length=50,label=_("title"))
+                          widget=forms.TextInput(attrs={'placeholder': u'לדוגמא, https://www.youtube.com/watch?v=2H4RkudFzlc',
+                                                        'class':'wideTextInput',
+                                                        'required':True}))
+    title = forms.CharField(max_length=70,label=_("title"),
+                            widget=forms.TextInput(attrs={'placeholder': u'לדוגמא, ״הדגמת תנועת מטוטלת, מורה - איתמר״',
+                                                          'class':'wideTextInput',
+                                                          'required':True}))
+
     content = forms.CharField(widget=Wysihtml5TextareaWidget(),label=_("content"))
     item = forms.ModelChoiceField(queryset=models.TeachItem.objects.all(),required=True, label=_("item"))
     tags = forms.ModelMultipleChoiceField(queryset=models.Tag.objects.all(),required=False, label=_("tags"))
