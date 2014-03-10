1c0d42635cbb9063e8822173575030a1162eaa4c
Yoni Yalovitsky <yoni@everything.me>
2014-03-10 23:43:35 +0200

    added user page

--- web/flipped/core/urls.py
+++ web/flipped/core/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^topic/(?P<topic_id>\d+)/$', views.topic_view, name='topic_view'),
     url(r'^item/(?P<item_id>\d+)/$', views.item_view, name='item_view'),
     url(r'^videos/rate/(?P<video_id>\d+)/$', views.video_rate, name='video_rate'),
+    url(r'^user/$', views.user_view, name='user_view'),
 )
