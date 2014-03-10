4547dae61ba7a8a0b5d8263f0abc33f794205821
Yoni Yalovitsky <yoni@everything.me>
2014-03-11 00:17:38 +0200

    added password change

--- web/flipped/fc_user/urls.py
+++ web/flipped/fc_user/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
-
-from fc_user import views
 from django.core.urlresolvers import reverse
+from fc_user import views
 
 urlpatterns = patterns('',
     #url(r'^$', views.index, name='index'),
@@ -10,7 +9,13 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login',kwargs=dict(template_name = 'fc_user/login.html'),name="login"),
     url(r'^logout/$', 'django.contrib.auth.views.logout',name="logout",kwargs=dict(next_page='/common/home/')),
     url(r'^register/$', views.register,name="register"),
-    url(r'^trans/$', views.trans,name="trans")       
+    url(r'^password/change/$', 'django.contrib.auth.views.password_change',
+        {'template_name': 'fc_user/password_change.html',
+         'post_change_redirect': '/fc_user/password/change/done/'},
+        name="password_change"),
+    url(r'^password/change/done/$', 'django.contrib.auth.views.password_change_done',
+    {'template_name': 'fc_user/password_change_done.html'},
+    name="password_change_done"),
 )
 #{'next_page' : reverse('common:home')}
 
