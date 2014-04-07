b128d64f71750089f75f29072bebbc4386bb1606
Barak Peleg <pelegb@gmail.com>
2014-04-07 22:45:11 +0300

    add feed

--- web/flipped/common/processors.py
+++ web/flipped/common/processors.py
@@ -4,5 +4,6 @@ def topics(request):
     parent_topics = core.models.TeachTopic.objects.filter(parent__isnull=True)
     return dict(topics=parent_topics)
 
-
+def feed(request):
+    return {'feed': core.models.VideoPage.objects.order_by('-upload_date')[:5]}
 
--- web/flipped/flipped/settings.py
+++ web/flipped/flipped/settings.py
@@ -110,6 +110,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR,'locale'),)
 from django.conf import global_settings
 TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 	"common.processors.topics",
+    "common.processors.feed",
 )
 
 try:
