d3ea2e8cf7d203a553773ed11a053edb5f657006
Yoni Yalovitsky <yoni@everything.me>
2014-07-07 22:58:26 +0300

    refactored get_ancestry_from_entity into the model, for consistency

--- web/flipped/common/utils.py
+++ web/flipped/common/utils.py
@@ -6,14 +6,3 @@ YOUTUBE_RE = re.compile('^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\
 
 def parse_video_id_from_link(link):
     return YOUTUBE_RE.match(link).group(1)
-
-
-def get_ancestry_from_entity(entity):
-#gets an entity and returns ancestor list sorted from highest to lowest, not including entity
-    ancestors=[]
-    while entity:
-        ancestors.insert(0, entity)
-        entity = entity.parent
-    return ancestors
-
-
