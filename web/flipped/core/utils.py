cfd9cc70014b72fdf4f3922b7564974f28924888
Barak Peleg <pelegb@gmail.com>
2014-12-29 23:13:17 +0200

    fix add video page

--- web/flipped/core/utils.py
+++ web/flipped/core/utils.py
@@ -8,6 +8,7 @@ def get_jstree_data(subtree, topic_id, opened=True, enable_items_only=False, inc
                   'text': u'%s (%s)' % (unicode(entity), entity.video_count()) if include_video_count else unicode(entity),
                   'a_attr': {'href': reverse('core:topic_view' if entity.entity_type == 'topic' else 'core:item_view', args=(entity.id,))},
                   'state': {'opened': opened,
-                            'disabled': enable_items_only if entity.entity_type == 'topic' else False}}
+                            'disabled': enable_items_only if entity.entity_type == 'topic' else False},
+                  'real_id': entity.id}
                  for entity in subtree]
     return tree_data
