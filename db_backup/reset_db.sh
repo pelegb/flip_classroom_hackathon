e412881585d4fb22abd26cf97be3fa8e746fe7b3
Barak Peleg <barak@thumzap.com>
2015-03-02 22:00:45 +0200

    allow reset db to receive external parameters

--- db_backup/reset_db.sh
+++ db_backup/reset_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export  PGPASSWORD=flipped
-dropdb flipped -U flipped 
-createdb flipped -U flipped
-psql -f db_dump.sql -U flipped # Restore the dumped data to the newly created DB
+dropdb flipped -U flipped $@
+createdb flipped -U flipped $@
+psql -f db_dump.sql -U flipped $@ # Restore the dumped data to the newly created DB
 
