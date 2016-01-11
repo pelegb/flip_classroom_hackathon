abcafc3d55eac63441b5a0fa6cbd5d0ca28e0b1b
Barak Peleg <barak@thumzap.com>
2016-01-11 22:10:18 +0200

    better mobile support for homepage

--- web/flipped/common/views.py
+++ web/flipped/common/views.py
@@ -3,7 +3,7 @@ import core.models
 
 
 def home(request):
-    feed = core.models.VideoPage.objects.order_by('-upload_date')[:5]
+    feed = core.models.VideoPage.objects.order_by('-upload_date')[:6]
     return render(request, 'common/home.html', {'feed': feed})
 
 
