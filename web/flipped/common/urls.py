12351ed9f914dcaceb3e0b9d140ec3e0acaae9c1
Barak Peleg <barak@thumzap.com>
2016-06-06 21:28:26 +0300

    set navbar logo click url

--- web/flipped/common/urls.py
+++ web/flipped/common/urls.py
@@ -5,7 +5,7 @@ from common import views
 urlpatterns = patterns('',
     # url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^home/$', views.home),
+    url(r'^home/$', views.home, name='home'),
     url(r'^about_us/$', views.about_us, name='about_us'),
     url(r'^help/$', views.help, name='help')
 )
