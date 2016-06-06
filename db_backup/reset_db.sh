49df8d53146e5bdc37e9824b831789360ecc163d
Barak Peleg <barak@thumzap.com>
2016-06-06 20:59:40 +0300

    fix reset db to not fail if db doesn't exist

--- db_backup/reset_db.sh
+++ db_backup/reset_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export  PGPASSWORD=flipped
-dropdb flipped -U flipped $@
+dropdb flipped -U flipped --if-exists $@
 createdb flipped -U flipped $@
 psql -f db_dump.sql -U flipped $@ # Restore the dumped data to the newly created DB
 
