5c8175f6cf843f7c4570406d0deca288a9b68256
Barak Peleg <barak@thumzap.com>
2017-05-23 10:37:01 +0300

    fetchcandidates script

--- web/flipped/common/utils.py
+++ web/flipped/common/utils.py
@@ -1,8 +1,14 @@
+import codecs
+import csv
+
+import cStringIO
+
 from flipped.settings import GOOGLE_API_KEY
 import re
 import requests
 
-YOUTUBE_RE = re.compile('^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$')
+YOUTUBE_RE = re.compile(
+    '^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$')
 
 
 def parse_video_id_from_link(link):
@@ -10,5 +16,43 @@ def parse_video_id_from_link(link):
 
 
 def request_youtube_info(video_id, part='id'):
-    r = requests.get('https://www.googleapis.com/youtube/v3/videos', params={'key': GOOGLE_API_KEY, 'part': part, 'id': video_id})
+    r = requests.get('https://www.googleapis.com/youtube/v3/videos',
+                     params={'key': GOOGLE_API_KEY, 'part': part, 'id': video_id})
     return r.json()
+
+
+def request_youtube_related_videos(video_id):
+    r = requests.get('https://www.googleapis.com/youtube/v3/search',
+                     params={'key': GOOGLE_API_KEY, 'part': 'snippet', 'type': 'video', 'relatedToVideoId': video_id,
+                             'relevanceLanguage': 'he', 'regionCode': 'IL'})
+    return r.json()
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
---
+++ web/flipped/core/management/commands/fetchcandidates.py
@@ -0,0 +1,39 @@
+import csv
+
+import progressbar
+from django.core.management import BaseCommand
+
+from common.utils import request_youtube_related_videos, UnicodeWriter
+from core.models import VideoPage
+
+
+class Command(BaseCommand):
+    help = 'Fetches candidate videos from YouTube\'s API'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--output-file', dest='output_file', default='output.csv', help='Output CSV file')
+
+    def handle(self, *args, **options):
+        with open(options['output_file'], 'wb') as f:
+            writer = UnicodeWriter(f)
+            writer.writerow(['Video Id', 'Published At', 'Title', 'Description', 'Channel'])
+
+            related_video_ids = set()
+            video_count = VideoPage.objects.count()
+            with progressbar.ProgressBar(max_value=video_count) as bar:
+                for i, video in zip(range(video_count), VideoPage.objects.all()):
+                    related_videos = request_youtube_related_videos(video.youtube_movie_id)
+                    for related_video in related_videos['items']:
+                        related_video_id = related_video['id']['videoId']
+                        if related_video_id not in related_video_ids:
+                            related_video_ids.add(related_video_id)
+                            try:
+                                VideoPage.objects.get(youtube_movie_id=related_video_id)
+                            except VideoPage.DoesNotExist:
+                                writer.writerow([related_video_id, related_video['snippet']['publishedAt'],
+                                                 related_video['snippet']['title'],
+                                                 related_video['snippet']['description'],
+                                                 related_video['snippet']['channelTitle']])
+                            except:
+                                pass
+                    bar.update(i)
