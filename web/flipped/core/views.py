3557561a3b89a95f9b4324fc09473523757b10d7
Barak Peleg <barakp@ai21.com>
2018-07-23 21:25:34 +0300

    fix bug if no cookie set

--- web/flipped/core/views.py
+++ web/flipped/core/views.py
@@ -58,8 +58,8 @@ def video_detail(request, video_id):
                            {'@type': 'ListItem', 'position': len(ancestors) + 1, 'item': {'@id': reverse('core:video_detail', args=(video.id,)), 'name': unicode(video)}}]}]
     ctx['ld_json'] = json.dumps(structured_data, cls=DjangoJSONEncoder)[1:-1]
     candidate_videos_query = video.candidate_videos
-    if request.session.get('rated_candidates', []):
-        candidate_videos_query = candidate_videos_query.exclude(id__in=request.session.get('rated_candidates', []))
+    if request.session.get('rated_candidates', False):
+        candidate_videos_query = candidate_videos_query.exclude(id__in=request.session.get('rated_candidates'))
     ctx['candidate_video'] = candidate_videos_query.first()
     return render(request, 'core/video_detail.html', ctx)
 
